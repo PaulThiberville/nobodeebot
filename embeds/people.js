21a21a1a60039bee1403ba2f332042fece2c4dfd
Paul Thiberville <paul.thiberville@live.fr>
2024-03-14 09:59:44 +0100

    people embed displaying displayName instead of username

--- embeds/people.js
+++ embeds/people.js
@@ -1,5 +1,6 @@
 exports.peopleEmbed = (people) => {
   const { id, avatar, displayName } = people.user;
+  console.log("user:", JSON.stringify(people.user, null, 2));
   const avatarUrl = `https://cdn.discordapp.com/avatars/${id}/${avatar}.png`;
   return {
     author: {
