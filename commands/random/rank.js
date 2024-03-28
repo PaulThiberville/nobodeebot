33c951c56b0827dec0e933f50e91fb6b8673e0a6
Paul Thiberville <paul.thiberville@live.fr>
2024-03-28 10:50:04 +0100

    adding role optional filter to rank command

--- commands/random/rank.js
+++ commands/random/rank.js
@@ -4,16 +4,32 @@ const { peopleEmbed } = require("../../embeds/people");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("rank")
-    .setDescription("Rank users in your voice channel"),
+    .setDescription("Rank users in your voice channel")
+    .addRoleOption((option) => {
+      return option
+        .setName("role")
+        .setDescription("Role to filter")
+        .setAutocomplete(true)
+        .setRequired(false);
+    }),
   async execute(interaction) {
     const voiceChannel = interaction.member.voice.channel;
     if (!voiceChannel) {
       return interaction.reply("You should be in a voice channel");
     }
 
-    const shuffledMembers = voiceChannel.members.sort(
+    const role = interaction.options.getRole("role");
+
+    let shuffledMembers = voiceChannel.members.sort(
       (a, b) => 0.5 - Math.random()
     );
+
+    if (role) {
+      const roleMembers = shuffledMembers.filter((member) =>
+        member.roles.cache.has(role.id)
+      );
+      shuffledMembers = roleMembers;
+    }
 
     const embeds = shuffledMembers.map((people) => {
       return peopleEmbed(people);
